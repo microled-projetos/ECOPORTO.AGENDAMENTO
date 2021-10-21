d4bcaffe34cbb0892d3d3e63ba5f4117f8e0b29c
microled-projetos <dropbox@microled.com.br>
2021-10-21 17:22:37 -0300

    fabio

--- src/Ecoporto.AgendamentoConteiner/src/Ecoporto.AgendamentoConteiner/App_Start/UnityConfig.cs
+++ src/Ecoporto.AgendamentoConteiner/src/Ecoporto.AgendamentoConteiner/App_Start/UnityConfig.cs
@@ -1,10 +1,12 @@
-using Ecoporto.AgendamentoConteiner.Dados.Interfaces;
-using Ecoporto.AgendamentoConteiner.Dados.Repositorios;
 using System;
+
 using Unity;
 
 namespace Ecoporto.AgendamentoConteiner
 {
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
     public static class UnityConfig
     {
         #region Unity Container
@@ -22,15 +24,24 @@ namespace Ecoporto.AgendamentoConteiner
         public static IUnityContainer Container => container.Value;
         #endregion
 
+        /// <summary>
+        /// Registers the type mappings with the Unity container.
+        /// </summary>
+        /// <param name="container">The unity container to configure.</param>
+        /// <remarks>
+        /// There is no need to register concrete types such as controllers or
+        /// API controllers (unless you want to change the defaults), as Unity
+        /// allows resolving a concrete type even if it was not previously
+        /// registered.
+        /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
-        {            
-            container.RegisterType<IUsuarioRepositorio, UsuarioRepositorio>();
-            container.RegisterType<IMotoristaRepositorio, MotoristaRepositorio>();
-            container.RegisterType<IVeiculoRepositorio, VeiculoRepositorio>();
-            container.RegisterType<IAgendamentoRepositorio, AgendamentoRepositorio>();
-            container.RegisterType<IReservaRepositorio, ReservaRepositorio>();
-            container.RegisterType<IEmpresaRepositorio, EmpresaRepositorio>();
-            container.RegisterType<IUploadRepositorio, UploadRepositorio>();
+        {
+            // NOTE: To load from web.config uncomment the line below.
+            // Make sure to add a Unity.Configuration to the using statements.
+            // container.LoadConfiguration();
+
+            // TODO: Register your type's mappings here.
+            // container.RegisterType<IProductRepository, ProductRepository>();
         }
     }
 }
